d24def3d5f8c0dd8c53bad80213d28b3c69623bf
King Honeycuttz <133569018+King-Honeycuttz@users.noreply.github.com>
2024-02-12 21:06:23 -0500

    Init Upload

---
+++ script/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('scroll', () => {
+    const nav = document.querySelector('nav');
+
+    if (window.scrollY > 0) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
+
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+
+};
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+};
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+};
